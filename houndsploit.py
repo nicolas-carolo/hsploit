3642f7e53fbd9e9ef1e84bd7d7923e7dc29bd883
Nicolas Carolo <nicolas.carolo@outlook.com>
2019-03-13 17:03:53 +0100

    Now user can open the source code

--- houndsploit.py
+++ houndsploit.py
@@ -2,6 +2,9 @@ import sys
 from searcher.engine.search_engine import search_vulnerabilities_in_db
 from searcher.db_manager.result_set import print_result_set, result_set_len
 from tools.colors import O, W
+from searcher.db_manager.session_manager import start_session
+from searcher.db_manager.models import Exploit, Shellcode
+import os
 
 
 def main(argv):
@@ -9,8 +12,27 @@ def main(argv):
         print('usage: python houndsploit.py "[text to search]"')
         exit(0)
 
-    searched_text = argv[0]
+    if argv[0] == '-oe' and not argv[1] is None:
+        session = start_session()
+        queryset = session.query(Exploit).filter(Exploit.id == argv[1])
+        session.close()
+        try:
+            os.system('nano ' + './searcher/vulnerabilities/' + queryset[0].file)
+        except IndexError:
+            print('ERROR: Exploit not found!')
+        return exit(0)
 
+    if argv[0] == '-os' and not argv[1] is None:
+        session = start_session()
+        queryset = session.query(Shellcode).filter(Shellcode.id == argv[1])
+        session.close()
+        try:
+            os.system('nano ' + './searcher/vulnerabilities/' + queryset[0].file)
+        except IndexError:
+            print('ERROR: Shellcode not found!')
+        return exit(0)
+
+    searched_text = argv[0]
     exploits_result_set = search_vulnerabilities_in_db(searched_text, 'searcher_exploit')
     shellcodes_result_set = search_vulnerabilities_in_db(searched_text, 'searcher_shellcode')
     print(str(result_set_len(exploits_result_set)) + ' exploits and ' + str(result_set_len(shellcodes_result_set)) + ' shellcodes found.\n')
