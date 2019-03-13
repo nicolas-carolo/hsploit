58eb5095afa14a098909c0f595e561533e4906a3
Nicolas Carolo <nicolas.carolo@outlook.com>
2019-03-13 16:25:25 +0100

    Results are displayed in a table with color

--- houndsploit.py
+++ houndsploit.py
@@ -1,6 +1,7 @@
 import sys
 from searcher.engine.search_engine import search_vulnerabilities_in_db
 from searcher.db_manager.result_set import print_result_set, result_set_len
+from tools.colors import O, W
 
 
 def main(argv):
@@ -14,10 +15,10 @@ def main(argv):
     shellcodes_result_set = search_vulnerabilities_in_db(searched_text, 'searcher_shellcode')
     print(str(result_set_len(exploits_result_set)) + ' exploits and ' + str(result_set_len(shellcodes_result_set)) + ' shellcodes found.\n')
     if result_set_len(exploits_result_set) > 0:
-        print('Exploits:')
+        print(O + 'EXPLOITS:' + W)
         print_result_set(exploits_result_set)
     if result_set_len(shellcodes_result_set) > 0:
-        print('\nShellcodes:')
+        print('\n' + O + 'SHELLCODES:' + W)
         print_result_set(shellcodes_result_set)
 
 
