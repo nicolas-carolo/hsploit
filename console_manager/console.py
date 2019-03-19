e05e2cee47afd6fe4647ce32d580e6a7a9d9d8dd
Nicolas Carolo <nicolas.carolo@outlook.com>
2019-03-19 22:48:01 +0100

    Software information updated

--- console_manager/console.py
+++ console_manager/console.py
@@ -24,8 +24,8 @@ def print_guide():
 
 def print_software_information():
     print_ascii_art('Hound\nSploit')
-    print(tabulate([[O + 'Version:' + W, '1.0.0 (Bash Version)'],
-                    [O + 'Release date:' + W, 'March 15, 2019'],
+    print(tabulate([[O + 'Version:' + W, '1.1.0 (Bash Version)'],
+                    [O + 'Release date:' + W, 'March 19, 2019'],
                     [O + 'Developer:' + W, 'Nicolas Carolo'],
                     [O + 'Last Database update:' + W, 'March 15, 2019']], tablefmt='grid'))
     exit(0)
