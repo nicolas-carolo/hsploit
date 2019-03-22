3e74c0fe479d3a049e686fad243393788bc461b1
Nicolas Carolo <nicolas.carolo@outlook.com>
2019-03-22 17:00:45 +0100

    1.2.1: Last DB update on March 22, 2019

--- console_manager/console.py
+++ console_manager/console.py
@@ -7,10 +7,10 @@ from searcher.engine.updates import is_update_available, download_update
 
 
 # Software information constants
-SW_VERSION = '1.2.0 (Bash Version)'
-RELEASE_DATE = 'March 21, 2019'
+SW_VERSION = '1.2.1 (Bash Version)'
+RELEASE_DATE = 'March 22, 2019'
 DEVELOPER = 'Nicolas Carolo'
-LAST_DB_UPDATE = 'March 15, 2019'
+LAST_DB_UPDATE = 'March 22, 2019'
 
 
 def print_guide():
