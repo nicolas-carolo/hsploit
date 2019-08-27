bcf1e5724c0ac87412e49d507c1864b9a8a4f5b8
Nicolas Carolo <nicolas.carolo@outlook.com>
2019-08-27 18:29:38 +0200

    1.2.10: Last My SQL version

--- searcher/db_manager/session_manager.py
+++ searcher/db_manager/session_manager.py
@@ -8,5 +8,6 @@ def start_session():
     :return: a new database session.
     """
     engine = sqlalchemy.create_engine('mysql+pymysql://hound-user:Hound-password9@localhost:3306/HOUNDSPLOIT')
+    # engine = sqlalchemy.create_engine('sqlite:///hound_db.sqlite3')
     Session = sessionmaker(bind=engine)
     return Session()
